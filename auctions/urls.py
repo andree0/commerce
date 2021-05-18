5cf798f9f5f24210713601a87acfa98aa17fe769
andree0 <jonek.andree@gmail.com>
2021-05-18 14:41:02 +0200

    change path media

--- auctions/urls.py
+++ auctions/urls.py
@@ -28,7 +28,6 @@ urlpatterns = [
          name="your_auctions"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT
-                          )
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT
+                      )
